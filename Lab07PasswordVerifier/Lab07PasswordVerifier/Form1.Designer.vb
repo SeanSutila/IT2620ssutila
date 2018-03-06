47da5026d35f64b89e68c927a908d2574b2e7bac
GodofBakon <ssutila@yahoo.com>
2018-03-05 20:25:39 -0500

    password verification app is complete

---
+++ Lab07PasswordVerifier/Lab07PasswordVerifier/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnVerify = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(52, 72)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(221, 20)
+        Me.txtPassword.TabIndex = 0
+        '
+        'btnVerify
+        '
+        Me.btnVerify.Location = New System.Drawing.Point(52, 126)
+        Me.btnVerify.Name = "btnVerify"
+        Me.btnVerify.Size = New System.Drawing.Size(91, 23)
+        Me.btnVerify.TabIndex = 1
+        Me.btnVerify.Text = "Verify"
+        Me.btnVerify.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(182, 126)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(91, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(125, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Enter Password"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 213)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnVerify)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Name = "Form1"
+        Me.Text = "Password Verification"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnVerify As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Lab07PasswordVerifier/Lab07PasswordVerifier/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnVerify_Click(sender As Object, e As EventArgs) Handles btnVerify.Click
+        If IsValid(txtPassword.Text) Then
+            MessageBox.Show("Great Job")
+        End If
+    End Sub
+
+    Private Function IsValid(strPassword As String) As Boolean
+        If strPassword = String.Empty Then
+            MessageBox.Show("Enter a password")
+            Return False
+        End If
+
+        If strPassword.Length < 6 Then
+            MessageBox.Show("Password must be 6 characters long")
+            Return False
+        End If
+        Dim dblx As Double
+        If Double.TryParse(strPassword, dblx) Then
+            MessageBox.Show("Password must have at least one alphabetic letter")
+            Return False
+        End If
+        Dim count As Integer = 0
+        While (count < (strPassword.Length))
+            If IsNumeric(strPassword.Substring(count, 1)) Then
+                Return True
+            End If
+            count += 1
+        End While
+        If count >= strPassword.Length Then
+            MessageBox.Show("Password must have at least on number in it")
+            Return False
+        End If
+        Return True
+    End Function
+
+End Class
