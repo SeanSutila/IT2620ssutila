740778ddd962d54682b780c8418d7bc25d138d3f
GodofBakon <ssutila@yahoo.com>
2018-03-12 23:54:21 -0400

    Travel Expenses app is somewhat done

---
+++ Lab08TravelExpenses/Lab08TravelExpenses/Form1.Designer.vb
@@ -0,0 +1,391 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDays = New System.Windows.Forms.TextBox()
+        Me.txtAirfare = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMeals = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtRental = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtMiles = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtParking = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTaxi = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtRegistration = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtLodging = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.lblTotalExpenses = New System.Windows.Forms.Label()
+        Me.lblTotalAllowed = New System.Windows.Forms.Label()
+        Me.lblTotalUnpaid = New System.Windows.Forms.Label()
+        Me.lblTotalSaved = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Days on trip:"
+        '
+        'txtDays
+        '
+        Me.txtDays.Location = New System.Drawing.Point(85, 24)
+        Me.txtDays.Name = "txtDays"
+        Me.txtDays.Size = New System.Drawing.Size(67, 20)
+        Me.txtDays.TabIndex = 1
+        Me.txtDays.Text = "0"
+        '
+        'txtAirfare
+        '
+        Me.txtAirfare.Location = New System.Drawing.Point(85, 50)
+        Me.txtAirfare.Name = "txtAirfare"
+        Me.txtAirfare.Size = New System.Drawing.Size(67, 20)
+        Me.txtAirfare.TabIndex = 3
+        Me.txtAirfare.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Airfare"
+        '
+        'txtMeals
+        '
+        Me.txtMeals.Location = New System.Drawing.Point(85, 76)
+        Me.txtMeals.Name = "txtMeals"
+        Me.txtMeals.Size = New System.Drawing.Size(67, 20)
+        Me.txtMeals.TabIndex = 5
+        Me.txtMeals.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Meals:"
+        '
+        'txtRental
+        '
+        Me.txtRental.Location = New System.Drawing.Point(260, 50)
+        Me.txtRental.Name = "txtRental"
+        Me.txtRental.Size = New System.Drawing.Size(67, 20)
+        Me.txtRental.TabIndex = 7
+        Me.txtRental.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(176, 53)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Car rental fees:"
+        '
+        'txtMiles
+        '
+        Me.txtMiles.Location = New System.Drawing.Point(260, 76)
+        Me.txtMiles.Name = "txtMiles"
+        Me.txtMiles.Size = New System.Drawing.Size(67, 20)
+        Me.txtMiles.TabIndex = 9
+        Me.txtMiles.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(189, 79)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Miles Driven"
+        '
+        'txtParking
+        '
+        Me.txtParking.Location = New System.Drawing.Point(260, 24)
+        Me.txtParking.Name = "txtParking"
+        Me.txtParking.Size = New System.Drawing.Size(67, 20)
+        Me.txtParking.TabIndex = 11
+        Me.txtParking.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(182, 27)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(72, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Parking Fees:"
+        '
+        'txtTaxi
+        '
+        Me.txtTaxi.Location = New System.Drawing.Point(457, 24)
+        Me.txtTaxi.Name = "txtTaxi"
+        Me.txtTaxi.Size = New System.Drawing.Size(67, 20)
+        Me.txtTaxi.TabIndex = 13
+        Me.txtTaxi.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(379, 27)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(72, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Taxi Charges:"
+        '
+        'txtRegistration
+        '
+        Me.txtRegistration.Location = New System.Drawing.Point(457, 50)
+        Me.txtRegistration.Name = "txtRegistration"
+        Me.txtRegistration.Size = New System.Drawing.Size(67, 20)
+        Me.txtRegistration.TabIndex = 15
+        Me.txtRegistration.Text = "0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(359, 53)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(92, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Registration Fees:"
+        '
+        'txtLodging
+        '
+        Me.txtLodging.Location = New System.Drawing.Point(457, 76)
+        Me.txtLodging.Name = "txtLodging"
+        Me.txtLodging.Size = New System.Drawing.Size(67, 20)
+        Me.txtLodging.TabIndex = 17
+        Me.txtLodging.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(361, 79)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(90, 13)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Lodging Charges:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(99, 237)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 30)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(330, 237)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 30)
+        Me.Button2.TabIndex = 19
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(74, 19)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(83, 13)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "Total Expenses:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(34, 55)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(123, 13)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "Total Expenses Allowed:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(314, 19)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(91, 13)
+        Me.Label12.TabIndex = 22
+        Me.Label12.Text = "Expenses unpaid:"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(325, 55)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(80, 13)
+        Me.Label13.TabIndex = 23
+        Me.Label13.Text = "Amount Saved:"
+        '
+        'lblTotalExpenses
+        '
+        Me.lblTotalExpenses.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalExpenses.Location = New System.Drawing.Point(163, 18)
+        Me.lblTotalExpenses.Name = "lblTotalExpenses"
+        Me.lblTotalExpenses.Size = New System.Drawing.Size(67, 20)
+        Me.lblTotalExpenses.TabIndex = 24
+        '
+        'lblTotalAllowed
+        '
+        Me.lblTotalAllowed.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalAllowed.Location = New System.Drawing.Point(163, 54)
+        Me.lblTotalAllowed.Name = "lblTotalAllowed"
+        Me.lblTotalAllowed.Size = New System.Drawing.Size(67, 20)
+        Me.lblTotalAllowed.TabIndex = 25
+        '
+        'lblTotalUnpaid
+        '
+        Me.lblTotalUnpaid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalUnpaid.Location = New System.Drawing.Point(411, 18)
+        Me.lblTotalUnpaid.Name = "lblTotalUnpaid"
+        Me.lblTotalUnpaid.Size = New System.Drawing.Size(67, 20)
+        Me.lblTotalUnpaid.TabIndex = 26
+        '
+        'lblTotalSaved
+        '
+        Me.lblTotalSaved.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalSaved.Location = New System.Drawing.Point(411, 54)
+        Me.lblTotalSaved.Name = "lblTotalSaved"
+        Me.lblTotalSaved.Size = New System.Drawing.Size(67, 20)
+        Me.lblTotalSaved.TabIndex = 27
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtLodging)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.txtRegistration)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.txtTaxi)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.txtParking)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txtMiles)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtRental)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtMeals)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtAirfare)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtDays)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(11, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(548, 115)
+        Me.GroupBox1.TabIndex = 28
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Trip Details"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblTotalSaved)
+        Me.GroupBox2.Controls.Add(Me.lblTotalUnpaid)
+        Me.GroupBox2.Controls.Add(Me.lblTotalAllowed)
+        Me.GroupBox2.Controls.Add(Me.lblTotalExpenses)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 127)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(545, 92)
+        Me.GroupBox2.TabIndex = 29
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Expenses"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(568, 304)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Travel Expenses"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDays As TextBox
+    Friend WithEvents txtAirfare As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtMeals As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtRental As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtMiles As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtParking As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtTaxi As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtRegistration As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtLodging As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents lblTotalExpenses As Label
+    Friend WithEvents lblTotalAllowed As Label
+    Friend WithEvents lblTotalUnpaid As Label
+    Friend WithEvents lblTotalSaved As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ Lab08TravelExpenses/Lab08TravelExpenses/Form1.vb
@@ -0,0 +1,136 @@
+﻿Public Class Form1
+
+    Dim intDays As Integer
+    Dim decMealReimburse As Decimal
+    Dim decMileReimburse As Decimal
+    Dim decParkReimburse As Decimal
+    Dim decTaxiFees As Decimal
+    Dim decLodgeReimburse As Decimal
+    Dim decTotalReimburse As Decimal
+    Dim decNotReimburse As Decimal
+    Dim decSaved As Decimal
+    Dim decTotalExpesses As Decimal
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not Integer.TryParse(txtDays.Text, intDays) Then
+            MessageBox.Show("Number of days not a valid whole number")
+            Exit Sub
+        ElseIf intDays < 1 Then
+            MessageBox.Show("Number of days must be more than one")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtAirfare.Text) Then
+            MessageBox.Show("Airfare must be a valid posotive number")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtMeals.Text) Then
+            MessageBox.Show("Meals must be a valid posotive number")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtParking.Text) Then
+            MessageBox.Show("Parking fees must be a valid posotive number")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtRental.Text) Then
+            MessageBox.Show("Rental fees must be a valid posotive number")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtMiles.Text) Then
+            MessageBox.Show("Miles must be a valid posotive number")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtTaxi.Text) Then
+            MessageBox.Show("Taxi charges must be a valid posotive number")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtRegistration.Text) Then
+            MessageBox.Show("Registration fees must be a valid posotive number")
+            Exit Sub
+        End If
+        If Not CheckPosNum(txtLodging.Text) Then
+            MessageBox.Show("Lodging must be a valid posotive number")
+            Exit Sub
+        End If
+
+        CalcTotalReimbersement()
+        lblTotalExpenses.Text = CalcTotalExpenses.ToString("C2")
+        lblTotalAllowed.Text = (intDays * 37 + intDays * 10 + intDays * 20 + intDays * 95).ToString("C2")
+        lblTotalSaved.Text = CalcSaved.ToString("C2")
+        lblTotalUnpaid.Text = Unallowed.ToString("C2")
+
+    End Sub
+
+    Private Function CheckPosNum(strcheck As String) As Boolean
+        Dim dblx As Double
+        If Not Double.TryParse(strcheck, dblx) Then
+            Return False
+        ElseIf dblx < 0 Then
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Function CalcMeals() As Decimal
+        Decimal.TryParse(txtMeals.Text, decMealReimburse)
+        If decMealReimburse > (intDays * 37) Then
+            decMealReimburse = intDays * 37
+        End If
+        Return decMealReimburse
+    End Function
+
+    Private Function CalcMileage() As Decimal
+        Decimal.TryParse(txtMiles.Text, decMileReimburse)
+        decMileReimburse = decMileReimburse * CDec(0.27)
+        Return decMileReimburse
+    End Function
+
+    Private Function CalcParkingFees() As Decimal
+        Decimal.TryParse(txtParking.Text, decParkReimburse)
+        If decParkReimburse > (intDays * 10) Then
+            decParkReimburse = intDays * 10
+        End If
+        Return decParkReimburse
+    End Function
+
+    Private Function CalcTaxiFees() As Decimal
+        Decimal.TryParse(txtTaxi.Text, decTaxiFees)
+        If decTaxiFees > (intDays * 20) Then
+            decTaxiFees = intDays * 20
+        End If
+        Return decTaxiFees
+    End Function
+
+    Private Function CalcLodging() As Decimal
+        Decimal.TryParse(txtLodging.Text, decLodgeReimburse)
+        If decLodgeReimburse > (intDays * 95) Then
+            decLodgeReimburse = intDays * 95
+        End If
+        Return decLodgeReimburse
+    End Function
+
+    Private Function CalcTotalReimbersement() As Decimal
+        decTotalReimburse = CalcMeals() + CalcMileage() + CalcParkingFees() + CalcTaxiFees() + CalcLodging()
+        Return decTotalReimburse
+    End Function
+
+    Private Function Unallowed() As Decimal
+        decNotReimburse = (intDays * 37 - decMealReimburse) + (intDays * 10 - decParkReimburse) + (intDays * 20 - decMileReimburse) + (intDays * 95 - decLodgeReimburse)
+        Return decNotReimburse
+    End Function
+
+    Private Function CalcSaved() As Decimal
+        decSaved = (decMealReimburse - intDays * 37) + (decParkReimburse - intDays * 10) + (decMileReimburse - intDays * 20) + (decLodgeReimburse - intDays * 95)
+        Return decSaved
+    End Function
+
+    Private Function CalcTotalExpenses() As Decimal
+        decTotalExpesses = CDec(txtAirfare.Text) + CDec(txtLodging.Text) + CDec(txtMeals.Text) + CDec(txtMiles.Text) + CDec(txtParking.Text) + CDec(txtRegistration.Text) + CDec(txtRental.Text) + CDec(txtTaxi.Text)
+        Return decTotalExpesses
+    End Function
+End Class
+
